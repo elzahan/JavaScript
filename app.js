99a462055dea48aa84d2454b0e0da8adb066b75f
elzahan <106437071+elzahan@users.noreply.github.com>
2022-09-15 15:11:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+//let f1 = () => console.log('-----')
+//f1()
+//let t1 = 10
+//let t2 = 15
+//let t3 = 20
+//let summ = (n1, n2) => {console.log(`Summ = ${n1 +n2}`)}
+//summ(t1, t2)
+//
+//function yolochka_pro () {
+//
+//}
+//yolochka_pro.prototype.method_1 = function(tt1, tt2) {
+//    console.log(`method_1 = ${tt1 + tt2}`)
+//}
+//
+//yolochka_pro.prototype.method_2 = function() {
+//    console.log('method_2')
+//}
+//
+//let wood = new yolochka_pro()
+
+//wood.method_1(10, 20)
+//wood.method_2()
+
+//let t1 = 7
+//let f3 = (t1 > 10 ) ?
+//(tt1, tt2) => console.log(tt1 + tt2):
+//(tt1, tt2) => console.log(tt1 - tt2);
+//f3(10, 30)
+
+//let item_1
+//item_1 = 5
+//console.log(item_1)
+//
+//let item_2
+//item_2 = 3
+//console.log(item_2)
+//
+//let item_3
+//item_3 = item_1 + item_2
+//console.log(item_3)
+//
+//let item_4
+//item_4 = 'Yolochka'
+//console.log(item_4)
+//
+//console.log(item_3 + item_4)
+//console.log(item_3 * item_4)
+//
+//let item_5
+//item_5 = item_3
+//
+//let item_6
+//let item_6_type
+
+let item_6 = 15
+let item_6_type = item_6
+console.log('item_6_type == ', typeof item_6_type) 
+console.log('item_6 == ', typeof item_6) 
+
+let item_7 = String(item_6)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+let item_7_type = item_7 
+console.log('item_7 == ', typeof item_7, 'item_7_type == ', typeof item_7) 
+
+let age1 = 10
+let age2 = 18
+let age3 = 60
+
+if (age1<age2) {
+    console.log('You don’t have access cause your age is' + age1 + 'It’s less then')
+} else if (age1 >= age2, age1 < age3) {
+    console.log('Welcome  !')
+} else  if (age1 >age3) {
+    console.log('Keep calm and look Culture channel')
+} else  {
+    console.log('Technical work')
+}
